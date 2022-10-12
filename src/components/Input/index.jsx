881c82571cce34c3d0c8bb9f2052577b43317aaa
Victor Andrade Sales <50026514+victor-sales@users.noreply.github.com>
2022-10-11 23:35:02 -0300

    Update index.jsx

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -9,7 +9,7 @@ export default function Input (props) {
             <Label htmlFor={id} text={labelText}/>
             <input
                 required={required}
-                className="border border-gray-300 rounded-md h-8 w-full"
+                className="border border-gray-300 rounded-md h-8 w-full p-2 outline-1 outline-gray-300 text-sm"
                 id={id}
                 type={type}
                 value={value}
