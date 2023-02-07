2e5ab7012eafd20f9766aa22e710e443d7737d2c
Victor Andrade Sales <50026514+victor-sales@users.noreply.github.com>
2023-02-06 23:17:55 -0300

    Layout

--- src/components/Header/HeaderLink.jsx
+++ src/components/Header/HeaderLink.jsx
@@ -4,7 +4,7 @@ export default function HeaderLink (props) {
     const { url, title } = props
     
     return (
-        <li>
+        <li className="">
             <Link href={url} >
                 <a className="text-gray-900 font-semibold hover:text-gray-700">{title}</a>
             </Link>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,7 @@
+import { faArrowDown, faCaretDown, faDog } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Modal } from "antd";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import useAuthContext from "../../hooks/useAuthContext";
@@ -20,11 +23,10 @@ export default function Header() {
             <div className="justify-between px-4 mx-auto items-center lg:max-w-7xl md:items-center md:flex md:px-8">
                 <div>
                     <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                        <a href="#">
-                            <h2 className="m-0 font-bold text-gray-900">
-                                LOGO
-                            </h2>
-                        </a>
+                        <FontAwesomeIcon
+                            className="h-6 w-6 text-gray-800"
+                            icon={faDog}
+                        />
                         <div className="md:hidden">
                             <button
                                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
@@ -68,7 +70,7 @@ export default function Header() {
                         className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
                             navbar ? "block" : "hidden"
                         }`}>
-                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 m-0">
+                        <ul className="items-center justify-center space-y-6 md:flex md:space-x-6 md:space-y-0 m-0">
                             {router.asPath === "/agendamento" ? <></> :
                             <>
                                 <HeaderLink url="/" title="Dashboard" />
@@ -80,36 +82,39 @@ export default function Header() {
                                 <HeaderLink url="/produtos" title="Produtos" />
                                 <HeaderLink url="/vendas" title="Vendas" /></>}
                         </ul>
-                        <div className="mt-3 space-y-2 lg:hidden md:hidden flex">
+                        <div className="mt-4 space-y-2 lg:hidden md:hidden flex flex-col items-center gap-3">
                             <button
                                 onClick={() => setModalVisible(true)}
-                                className="inline-block w-full px-4 py-2 text-center text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
-                            >
-                                Sign out
+                                className="inline-block w-full p-2 text-center text-white bg-gray-600 rounded-md shadow hover:bg-gray-800">
+                                Sair
                             </button>
-                            <div className="">
-                                <button className="text-sm text-black">Alterar Senha</button>
-                            </div>
+                            <button className="inline-block w-full p-2 text-sm text-gray-900 border border-gray-900 rounded-md">Alterar Senha</button>
                         </div>
                         
                     </div>
                 </div>
                 
                 <div className="hidden space-x-2 md:flex md:items-center">
-                    <div className="relative mr-3">
-                            <button onClick={() => setChangePassword(!changePassword)} className="text-sm text-black">Alterar Senha</button>
-                        
+                    <div className="relative mr-2 border-r border-gray-900">
+                           
+                            <button onClick={() => setChangePassword(!changePassword)} className="text-sm text-gray-900 flex items-center gap-2 pr-3">
+                                <FontAwesomeIcon
+                                    className="h-4 w-4"
+                                    icon={faCaretDown}
+                                />
+                                <span>Alterar Senha</span>
+                            </button>
                         {changePassword ? 
                             <div className="absolute z-10 bg-white p-2 border border-gray-300 rounded-md top-5 right-10 w-60">
                                 <ChangePasswordForm />
                             </div>
-                            : <></>}
+                            : <></>
+                        }
                             </div>
                     <button
                         onClick={() => setModalVisible(true)}                        
-                        className="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
-                    >
-                        Sign Out
+                        className="px-4 py-1 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800">
+                        Sair
                     </button>
                     
                 </div>
@@ -123,7 +128,7 @@ export default function Header() {
                 title="Sair"
                 open={modalVisible}>
                     <ConfirmSignOutText />
-                </Modal>
+            </Modal>
         </nav>
     );
 }
